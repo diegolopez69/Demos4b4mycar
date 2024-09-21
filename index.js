20e9f85bf7336af73e596b7419e9a88040f93d46
Diego Lopez <diegolopezselva@gmail.com>
2024-09-21 14:13:34 +0200

    Change the db bc it was a mess with the connection to Mongo Atlas

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-import cors from 'cors';
 import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 import demoRouter from './demo/routes.js';
 import userRouter from './user/routes.js';
-import mongoose from 'mongoose';
-dotenv.config();
 
+dotenv.config();
 const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json()); // Configura body-parser para manejar JSON
+app.use(bodyParser.urlencoded({ extended: true })); // Configura body-parser para manejar formularios codificados en URL
+
+
+// Parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// Parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
@@ -14,21 +27,24 @@ demoRouter(app)
 userRouter(app)
 //db
 
-const MONGODB_URI = process.env.MONGODB_URI
+// Connection to the local MongoDB database
+const MONGODB_URI = process.env.MONGODB_URI;
 
-if(MONGODB_URI){
-  mongoose.connect(MONGODB_URI)
-  .then(()=>{
-
-  })
-  .catch(error=>{
-    console.log("Configuración incorrecta.")
-  process.exit(-1)
-  })
-}else{
-  console.log("Falta configuraciones para conectar.")
-  process.exit(-1)
+if (MONGODB_URI) {
+  mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log("Connected to the local 'bemycar' MongoDB database.");
+    })
+    .catch(error => {
+      console.log("Error in database configuration:", error);
+      process.exit(-1);
+    });
+} else {
+  console.log("Missing configurations to connect to the database.");
+  process.exit(-1);
 }
+
+
 
 // simple route
 app.get("/", (req, res) => {
--- user/model.js
+++ user/model.js
@@ -1,10 +1,10 @@
 import { Schema, model  } from "mongoose";
 
 const UserSchema = new Schema({
-    name: String, 
-    surnames: String, 
-    userName: String, 
-    password: String
+    name: {type: String, required: false},
+    surnames: {type: String, required: false}, 
+    userName: {type: String, required: true},
+    password: {type: String, required: true},
 }
 )
 const UserModel = model('User', UserSchema);
